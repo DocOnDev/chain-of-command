a54fc70dad3d82efede75c95b68f1992e842704b
Doc Norton <doc@docondev.com>
2019-05-14 19:37:04 -0400

    Move Werewolf specific attributes into Werewolf Evaluator Command

--- src/main/java/com/docondev/chain/suspect/InterestingFacts.java
+++ src/main/java/com/docondev/chain/suspect/InterestingFacts.java
@@ -1,13 +1,8 @@
 package com.docondev.chain.suspect;
 
-import java.util.Arrays;
-import java.util.List;
-
 class InterestingFacts {
     static final Double DUCK_WEIGHT = 4.2;
     static final double WATER_DENSITY = 8.333;  // lb/gal or 62.335 lb/ft3
-    static final List<String> canineAttributes = Arrays.asList("claws", "fur", "fangs");
-    static final List<String> warewolfHobbies = Arrays.asList("lycanthropy", "howling at the moon");
 
     private InterestingFacts() {
         throw new IllegalStateException("Data Utility class");
--- src/main/java/com/docondev/chain/suspect/WerewolfEvaluatorCommand.java
+++ src/main/java/com/docondev/chain/suspect/WerewolfEvaluatorCommand.java
@@ -1,17 +1,23 @@
 package com.docondev.chain.suspect;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class WerewolfEvaluatorCommand extends AbstractEvaluatorCommand {
+    private static final List<String> canineAttributes = Arrays.asList("claws", "fur", "fangs");
+    private static final List<String> werewolfHobbies = Arrays.asList("lycanthropy", "howling at the moon");
+
     @Override
     protected boolean shouldDetain(Suspect suspect) {
         return isCanineLike(suspect) && isLycanthrope(suspect);
     }
 
     private static boolean isCanineLike(Suspect suspect) {
-        return suspect.getProperties().stream().anyMatch(InterestingFacts.canineAttributes::contains);
+        return suspect.getProperties().stream().anyMatch(canineAttributes::contains);
     }
 
     private static boolean isLycanthrope(Suspect suspect) {
-        return suspect.getHobbies().stream().anyMatch(InterestingFacts.warewolfHobbies::contains);
+        return suspect.getHobbies().stream().anyMatch(werewolfHobbies::contains);
     }
 
 }
